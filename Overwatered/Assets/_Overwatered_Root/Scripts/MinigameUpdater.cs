cfc2ad9ef4cbd7fa9efbe7398b068da96fb409d8
elenanope <elenaromanmoreno@gmail.com>
2026-01-01 18:44:38 +0100

    Arreglo Animación Row Idle, Primera versión de vegetación

--- Overwatered/Assets/_Overwatered_Root/Scripts/MinigameUpdater.cs
+++ Overwatered/Assets/_Overwatered_Root/Scripts/MinigameUpdater.cs
@@ -27,5 +27,7 @@ public class MinigameUpdater : MonoBehaviour
             player.transform.rotation = GameManager.Instance.gameData.initialPlayerRot;
             GameManager.Instance.gameData.gameHasStarted = true;
         }
+        Debug.Log(GameManager.Instance.gameData.initialPlayerPos);
+        Debug.Log(GameManager.Instance.gameData.lastPlayerPos);
     }
 }
